f7f0129bb9f785e1349372a751900cfa0fa7d64f
Mateusz <matemusz@gmail.com>
2019-02-21 18:05:04 +0100

    add sout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.zip.CheckedOutputStream;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("JUPI");
     }
 }
+
